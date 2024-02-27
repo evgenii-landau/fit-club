62389bcb25cff5b28bd7abfc4f5888cf151d7ffa
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-27 09:06:45 +0300

    created join section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Programs} from "./components/Programs/Programs";
 import {Reasons} from "./components/Reasons/Reasons";
 import {Plans} from "./components/Plans/Plans";
 import {Testimonials} from "./components/Testimonials/Testimonials";
+import {Join} from "./components/Join/Join";
+
 
 function App() {
 	return (
@@ -13,6 +15,7 @@ function App() {
 			<Reasons/>
 			<Plans/>
 			<Testimonials/>
+			<Join/>
 		</div>
 	);
 }
---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './Join.css'
+import emailjs from '@emailjs/browser'
+
+export const Join = () => {
+	const form = React.useRef()
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm('service_0ditu7q', 'template_g5dy8rf', form.current, {
+				publicKey: 'acJvR0Ypk3rRlV46l',
+			})
+			.then(
+				() => {
+					console.log('SUCCESS!');
+				},
+				(error) => {
+					console.log('FAILED...', error.text);
+				},
+			);
+	};
+
+	return (
+		<div className="Join" id='join-us'>
+			<div className='left-j'>
+				<hr/>
+				<div>
+					<span className='stroke-text'>READY TO</span>
+					<span>LEVEL UP</span>
+				</div>
+				<div>
+					<span>YOUR BODY</span>
+					<span className='stroke-text'>WITH US?</span>
+				</div>
+			</div>
+			<div className='right-j'>
+				<form ref={form} action="" className='email-container' onSubmit={sendEmail}>
+					<input type="email" name='user_email' placeholder='Enter your Email address'/>
+					<button className='btn btn-j'>Join Now</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
