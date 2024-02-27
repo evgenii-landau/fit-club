8faf71c96382137faf8b6b2797d0063d307a8f24
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-27 08:33:33 +0300

    created plan section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import {Hero} from "./components/Hero/Hero";
 import {Programs} from "./components/Programs/Programs";
 import {Reasons} from "./components/Reasons/Reasons";
+import {Plans} from "./components/Plans/Plans";
+import {Testimonials} from "./components/Testimonials/Testimonials";
 
 function App() {
 	return (
@@ -9,6 +11,8 @@ function App() {
 			<Hero/>
 			<Programs/>
 			<Reasons/>
+			<Plans/>
+			<Testimonials/>
 		</div>
 	);
 }
